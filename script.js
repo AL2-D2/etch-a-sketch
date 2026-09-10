5df1898bcd49d5eac4994fb5ba129d6fe8526d28
AL2D2 <altug_cem2005@hotmail.com>
2026-09-10 18:04:38 +0300

    fully functional 'change button' added...

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector("#container");
+//default grid size
+const DEFAULT_GRID = 16;
 
 // Functions
-
 function setGridSize(desiredNumber) {
+    //in the beginning of set, reset every time the parent div...
+    container.innerHTML = "";
     for (let i = 0; i < desiredNumber; i++) {
         const newCol = document.createElement('div');
         newCol.classList.add("col");
@@ -13,10 +16,11 @@ function setGridSize(desiredNumber) {
         }
         container.appendChild(newCol);
     }
+    addEventsToBoxes();
 }
 
 //default grid size
-setGridSize(16);
+setGridSize(DEFAULT_GRID);
 
 //function that selects random color and returns arr of colors.. because in rgb() you use three different values
 function randomColor() {
@@ -33,16 +37,42 @@ function randomColor() {
     return color;
 }
 
-const boxes = document.querySelectorAll(".box");
+function addEventsToBoxes() {
+    const boxes = document.querySelectorAll(".box");
 
-boxes.forEach(box => {
-    /* whenever mouse hovers over the boxes new set of random rgb is assigned to the array and then three 
-    rgb values assigned to the background color one by one.*/
-    box.addEventListener("mouseover", () => {
-        const rgbValuesArr = randomColor();
-        const firstRgb = rgbValuesArr[0];
-        const secondRgb = rgbValuesArr[1];
-        const thirdRgb = rgbValuesArr[2];
-        box.style.backgroundColor = `rgb(${firstRgb}, ${secondRgb}, ${thirdRgb})`;
-    })
+    boxes.forEach(box => {
+        /* whenever mouse hovers over the boxes new set of random rgb is assigned to the array and then three 
+        rgb values assigned to the background color one by one.*/
+        box.addEventListener("mouseover", () => {
+            const rgbValuesArr = randomColor();
+            const firstRgb = rgbValuesArr[0];
+            const secondRgb = rgbValuesArr[1];
+            const thirdRgb = rgbValuesArr[2];
+            box.style.backgroundColor = `rgb(${firstRgb}, ${secondRgb}, ${thirdRgb})`;
+        })
+    });
+}
+
+//grid change button and its function
+function askGridSize() {
+    const gridSize = Number(prompt("Please enter your desired grid size: "));
+    if (typeof gridSize !== 'undefined' && gridSize !== null) {
+        if (gridSize > 100) {
+            alert("YOU CANNOT SELECT NUMBER HIGHER THAN 100");
+            return DEFAULT_GRID;
+        }
+        else {
+            return gridSize;
+        }
+    }
+    else {
+        alert("ERROR... Grid size is now 16 (default)");
+        return DEFAULT_GRID;
+    }
+}
+const gridButton = document.querySelector("#grid-btn");
+
+gridButton.addEventListener("click", () => {
+    const gridSize = askGridSize();
+    setGridSize(gridSize);
 });
