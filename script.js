6bf5ea27e428ff08f80202a742544f3f56283e3d
AL2D2 <altug_cem2005@hotmail.com>
2026-09-10 16:41:32 +0300

    Made grids 16x16 and they have borders rn

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("#container");
+
+// Functions
+
+function setGridSize(desiredNumber){
+    for(let i = 0; i < desiredNumber; i++){
+        const newCol = document.createElement('div');
+        newCol.classList.add("col");
+        for(let j = 0; j < desiredNumber; j++){
+            const newBox = document.createElement("div");
+            newBox.classList.add("box");            
+            newCol.appendChild(newBox);
+        }        
+        container.appendChild(newCol);        
+    }
+}
+
+setGridSize(16);
