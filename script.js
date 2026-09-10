697d23c898c4cbfb4d11bfbe4061bac38340a223
AL2D2 <altug_cem2005@hotmail.com>
2026-09-10 16:51:26 +0300

    made changes in script file that makes the background color of the boxes turn purple when hovered

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const container = document.querySelector("#container");
 
 // Functions
 
-function setGridSize(desiredNumber){
-    for(let i = 0; i < desiredNumber; i++){
+function setGridSize(desiredNumber) {
+    for (let i = 0; i < desiredNumber; i++) {
         const newCol = document.createElement('div');
         newCol.classList.add("col");
-        for(let j = 0; j < desiredNumber; j++){
+        for (let j = 0; j < desiredNumber; j++) {
             const newBox = document.createElement("div");
-            newBox.classList.add("box");            
+            newBox.classList.add("box");
             newCol.appendChild(newBox);
-        }        
-        container.appendChild(newCol);        
+        }
+        container.appendChild(newCol);
     }
 }
 
 setGridSize(16);
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(box => {
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "purple";
+    })
+});
